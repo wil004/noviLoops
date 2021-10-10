2d4adbfca09b8dc41ca1fb5929e6e18775e3b7cb
wil004 <Williammeester18@gmail.com>
2021-10-10 22:44:34 +0200

    loop homework novi

---
+++ loopsAndArrays.js
@@ -0,0 +1,62 @@
+// ==========================================
+// Opdracht 1
+// Schrijf een script dat iedere naam in bovenstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
+// ==========================================
+
+const names = ["Henk", "Piet", "Fred", "Joop"];
+
+for (let i = 0; i < names.length; i++) {
+   names[i] = names[i] + 'je';
+}
+
+console.log(names);
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
+// Na jouw script zie je de aangepaste waardes:
+// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+
+
+// ==========================================
+// Opdracht 2
+// Schrijf een script dat ieder getal in bovenstaande array met 3 vermenigvuldigd.
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+
+const numbers = [2, 4, 5, 29, 38];
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = (numbers[i] * 3);
+}
+
+console.log(numbers)
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(numbers) geeft: [2, 4, 5, 29, 38];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
+
+
+// ==========================================
+// Opdracht 3
+// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array het volume uitrekent.
+// Het volume is lengte x breedte x hoogte
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+
+const squares = [30, 2, 8, 24, 11];
+
+for (let i = 0; i < squares.length; i++) {
+    squares[i] = (squares[i] * squares[i] * squares[i]);
+}
+
+console.log(squares);
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(numbers) geeft: [30, 2, 8, 24, 11];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(numbers) geeft: [27000, 8, 512, 13824, 1331];
